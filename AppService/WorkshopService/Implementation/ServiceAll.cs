bc37288be4abe697aae9ee33a5ccda12c2729ca8
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-10-11 22:22:31 +0700

    minor fix

--- AppService/WorkshopService/Implementation/ServiceAll.cs
+++ AppService/WorkshopService/Implementation/ServiceAll.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Models.ServiceModels.WorkshopModels;
+using Models.ServiceModels.WorkshopModels.WorkshopClass;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace AppService.WorkshopService.Implementation
 {
     public class ServiceAll : IServiceAll
     {
+        public Task<IEnumerable<WorkshopClassViewModel>> GetWorkshopClassesByWorkshopId(int workshopId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WorkshopClassDetailViewModel> GetWorkshopClassSlotById(int workshopClassDetailId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<WorkshopClassDetailViewModel>> GetWorkshopClassSlotsByWorkshopClassId(int workshopClassId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<WorkshopModel>> GetWorkshops()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
