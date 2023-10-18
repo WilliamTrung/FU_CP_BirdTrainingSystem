65ce3f6ff96244f4bd469c2e9afc2912c700cb28
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-10-18 22:14:02 +0700

    inject features and services

---
+++ BirdTrainingCenterAPI/Controllers/Workshop/WorkshopCustomerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirdTrainingCenterAPI.Controllers.Workshop
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkshopCustomerController : ControllerBase
+    {
+    }
+}
