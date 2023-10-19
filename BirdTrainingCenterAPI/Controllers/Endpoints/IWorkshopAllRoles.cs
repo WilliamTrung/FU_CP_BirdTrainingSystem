1c0ac6a22f6e9fd5cbfc519c6ffa0daa93fc5353
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-10-19 17:32:06 +0700

    Add endpoint interfaces

---
+++ BirdTrainingCenterAPI/Controllers/Endpoints/IWorkshopAllRoles.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BirdTrainingCenterAPI.Controllers.Endpoints
+{
+    public interface IWorkshopAllRolesEndpoint
+    {
+        Task<IActionResult> GetWorkshopsAsync();
+        [Route("class")]
+        Task<IActionResult> GetClassesOnSelectedWorkshopAsync([FromQuery]int workshopId);
+        [Route("class-slots")]
+        Task<IActionResult> GetWorkshopClassDetailsAsync([FromQuery]int workshopClassId);
+        [Route("slot-detail")]
+        Task<IActionResult> GetClassSlotDetailsAsync([FromQuery]int workshopClassDetailId);
+        [Route("refund-policy")]
+        Task<IActionResult> GetWorkshopRefuncPolicy();
+    
+    
+    }
+}
--- BirdTrainingCenterAPI/Controllers/Workshop/WorkshopController.cs
+++ BirdTrainingCenterAPI/Controllers/Workshop/WorkshopController.cs
@@ -14,10 +14,43 @@ namespace BirdTrainingCenterAPI.Controllers.Workshop
         }
         //all roles
         /*
-         Get workshops
+        Get workshops
+        Get workshop classes on selected workshop
+        Get workshop class slot details on selected workshop class
+        Get workshop class slot detail
+        Get workshop policies
+
+         */
+        
+        //customer
+        /*
+         Register workshop class
+        Get registered workshop classes
+
          */
         //staff
+        /*
+         Create workshop class
+        Get workshop classes with admin view
+        Get workshop class details with admin view
+        "Modify workshop slot- change trainer, date and slot"
+        "Modify workshop slot- change date and slot"
+
+         */
         //manager
+        /*
+         Create workshop
+        Get workshop detail template
+        Modify workshop detail template
+        Modify workshop status
+        Get workshop status
+         */
         //trainer
+        /*
+         Get assigned workshops
+        Get assigned workshop classes
+        Get assigned workshop class details
+
+         */
     }
 }
