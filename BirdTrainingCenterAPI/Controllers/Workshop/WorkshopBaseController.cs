4b53463a792c52be82925392bc83f3a813463627
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-10-22 19:44:32 +0700

    Implement controller features

--- BirdTrainingCenterAPI/Controllers/Workshop/WorkshopBaseController.cs
+++ BirdTrainingCenterAPI/Controllers/Workshop/WorkshopBaseController.cs
@@ -1,6 +1,8 @@
-﻿using AppService.WorkshopService;
+﻿using AppService;
+using AppService.WorkshopService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BirdTrainingCenterAPI.Controllers.Workshop
 {
@@ -9,9 +11,22 @@ namespace BirdTrainingCenterAPI.Controllers.Workshop
     public class WorkshopBaseController : ControllerBase
     {
         internal readonly IWorkshopService _workshopService;
-        public WorkshopBaseController(IWorkshopService workshopService)
+        internal readonly IAuthService _authService;
+
+        public WorkshopBaseController(IWorkshopService workshopService, IAuthService authService)
         {
             _workshopService = workshopService;
+            _authService = authService;
+        }
+        internal List<Claim>? DeserializeToken()
+        {
+            var authHeader = Request.Headers["Authorization"];
+            if (authHeader.Count == 0 || !authHeader[0].StartsWith("Bearer "))
+            {
+                return null;
+            }
+            string accessToken = authHeader[0].Split(' ')[1];
+            return _authService.DeserializedToken(accessToken);
         }
     }
 }
--- BirdTrainingCenterAPI/Controllers/Workshop/WorkshopCustomerController.cs
+++ BirdTrainingCenterAPI/Controllers/Workshop/WorkshopCustomerController.cs
@@ -1,26 +1,42 @@
-﻿using AppService.WorkshopService;
+﻿using AppService;
+using AppService.WorkshopService;
 using BirdTrainingCenterAPI.Controllers.Endpoints.Workshop;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BirdTrainingCenterAPI.Controllers.Workshop
 {
     public class WorkshopCustomerController : WorkshopBaseController, IWorkshopCustomer
     {
-        public WorkshopCustomerController(IWorkshopService workshopService) : base(workshopService)
+        public WorkshopCustomerController(IWorkshopService workshopService, IAuthService authService) : base(workshopService, authService)
         {
         }
 
         public async Task<IActionResult> GetRegisteredClasses()
-        {            
-            var result = await _workshopService.Customer.GetRegisteredClasses(1);
+        {
+            var accessToken = DeserializeToken();
+            if(accessToken == null)
+            {
+                return Unauthorized();
+            }
+            var customerId = accessToken.First(c => c.Type == ClaimTypes.NameIdentifier);
+
+            var result = await _workshopService.Customer.GetRegisteredClasses(Int32.Parse(customerId.Value));
             return Ok(result);
 
         }
 
-        public Task<IActionResult> Register([FromQuery] int workshopClassId)
+        public async Task<IActionResult> Register([FromQuery] int workshopClassId)
         {
-            throw new NotImplementedException();
+            var accessToken = DeserializeToken();
+            if (accessToken == null)
+            {
+                return Unauthorized();
+            }
+            var customerId = accessToken.First(c => c.Type == ClaimTypes.NameIdentifier);
+            await _workshopService.Customer.Regsiter(Int32.Parse(customerId.Value), workshopClassId);
+            return Ok();
         }
     }
 }
