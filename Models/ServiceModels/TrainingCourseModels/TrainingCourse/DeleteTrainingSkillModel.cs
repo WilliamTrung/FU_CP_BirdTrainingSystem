04030187df090f1daeffcf6857e83511dfdc2600
anhtnse <trinhnamanh0909@gmail.com>
2023-11-04 17:10:12 +0700

    Modify register model due to client Add delete training skill model

---
+++ Models/ServiceModels/TrainingCourseModels/TrainingCourse/DeleteTrainingSkillModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ServiceModels.TrainingCourseModels.TrainingCourse
+{
+    public class DeleteTrainingSkillModel
+    {
+        public int BirdSkillId { get; set; }
+        public int TrainingCourseId { get; set; }
+    }
+}
