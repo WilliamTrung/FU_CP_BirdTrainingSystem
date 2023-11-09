0cfb0769ad2520b77643740a526e9cd2eeb331e5
quandoivietnam <trinhnamanh0909@gmail.com>
2023-11-09 11:34:44 +0700

    Update Models.Entities

---
+++ Models/Entities/BirdSkillReceived.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class BirdSkillReceived
+    {
+        public int BirdSKillId { get; set; }
+        public int BirdId { get; set; }
+        public DateTime DateReceive { get; set; }
+
+        public virtual Bird? Bird { get; set; }
+        public virtual BirdSkill? BirdSkill { get; set; }
+    }
+}
