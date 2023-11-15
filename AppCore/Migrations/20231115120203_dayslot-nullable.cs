80783949436079b2838970bf85ba152787a5f68f
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-11-15 19:02:48 +0700

    update db

---
+++ AppCore/Migrations/20231115120203_dayslot-nullable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppCore.Migrations
+{
+    public partial class dayslotnullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FKWorkshopCl382995",
+                table: "WorkshopClassDetail");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DaySlotId",
+                table: "WorkshopClassDetail",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FKWorkshopCl382995",
+                table: "WorkshopClassDetail",
+                column: "DaySlotId",
+                principalTable: "TrainerSlot",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FKWorkshopCl382995",
+                table: "WorkshopClassDetail");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DaySlotId",
+                table: "WorkshopClassDetail",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FKWorkshopCl382995",
+                table: "WorkshopClassDetail",
+                column: "DaySlotId",
+                principalTable: "TrainerSlot",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
