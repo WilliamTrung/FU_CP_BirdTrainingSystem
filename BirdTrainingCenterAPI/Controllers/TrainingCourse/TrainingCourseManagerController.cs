8b9dea0898df1afced7f540a7d03769329b5cd55
anhtnse <trinhnamanh0909@gmail.com>
2023-11-21 13:05:16 +0700

    Fix

--- BirdTrainingCenterAPI/Controllers/TrainingCourse/TrainingCourseManagerController.cs
+++ BirdTrainingCenterAPI/Controllers/TrainingCourse/TrainingCourseManagerController.cs
@@ -256,7 +256,7 @@ namespace BirdTrainingCenterAPI.Controllers.TrainingCourse
             var pictures = string.Empty;
             if(birdCertificateAddParam.Picture != null)
             {
-                if (birdCertificateAddParam.Picture.IsImage())
+                if (!birdCertificateAddParam.Picture.IsImage())
                 {
                     return BadRequest("Upload image only!");
                 }
