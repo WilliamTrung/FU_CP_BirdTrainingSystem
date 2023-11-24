cb6b5b6641503343edf56d97ef90e182611c865b
anhtnse <trinhnamanh0909@gmail.com>
2023-11-25 03:44:14 +0700

    remove auto change birdtrainingcourse status

--- AppRepository/Repository/Implement/BirdTrainingCourseRepository.cs
+++ AppRepository/Repository/Implement/BirdTrainingCourseRepository.cs
@@ -10,25 +10,25 @@ namespace AppRepository.Repository.Implement
         public BirdTrainingCourseRepository(AppCore.Context.BirdTrainingCenterSystemContext context, IUnitOfWork unitOfWork) : base(context, unitOfWork)
         {
         }
-        public override async Task<IEnumerable<BirdTrainingCourse>> Get(Expression<Func<BirdTrainingCourse, bool>>? expression = null, params string[] includeProperties)
-        {
-            var entities = await base.Get(expression, includeProperties);
-            foreach (var entity in entities)
-            {
-                if(entity != null)
-                {
-                    if (entity.StartTrainingDate != null)
-                    {
-                        int compareDate = DateTime.Compare((DateTime)entity.StartTrainingDate, DateTime.Now);
-                        if (entity.Status == (int)Models.Enum.BirdTrainingCourse.Status.CheckIn && compareDate >= 0)
-                        {
-                            entity.Status = (int)Models.Enum.BirdTrainingCourse.Status.Training;
-                            await Update(entity);
-                        }
-                    }
-                }
-            }
-            return entities;
-        }
+        //public override async Task<IEnumerable<BirdTrainingCourse>> Get(Expression<Func<BirdTrainingCourse, bool>>? expression = null, params string[] includeProperties)
+        //{
+        //    var entities = await base.Get(expression, includeProperties);
+        //    foreach (var entity in entities)
+        //    {
+        //        if (entity != null)
+        //        {
+        //            if (entity.StartTrainingDate != null)
+        //            {
+        //                int compareDate = DateTime.Compare(DateTime.Now, (DateTime)entity.StartTrainingDate);
+        //                if (entity.Status == (int)Models.Enum.BirdTrainingCourse.Status.CheckIn && compareDate >= 0)
+        //                {
+        //                    entity.Status = (int)Models.Enum.BirdTrainingCourse.Status.Training;
+        //                    await Update(entity);
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return entities;
+        //}
     }
 }
