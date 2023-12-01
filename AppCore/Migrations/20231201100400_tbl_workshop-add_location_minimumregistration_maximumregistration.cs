7b400e804f8aebace98a7b44d0f306c21e48584a
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-12-01 17:15:58 +0700

    Update database

---
+++ AppCore/Migrations/20231201100400_tbl_workshop-add_location_minimumregistration_maximumregistration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppCore.Migrations
+{
+    public partial class tbl_workshopadd_location_minimumregistration_maximumregistration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Workshop",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaximumRegistration",
+                table: "Workshop",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinimumRegistration",
+                table: "Workshop",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Workshop");
+
+            migrationBuilder.DropColumn(
+                name: "MaximumRegistration",
+                table: "Workshop");
+
+            migrationBuilder.DropColumn(
+                name: "MinimumRegistration",
+                table: "Workshop");
+        }
+    }
+}
