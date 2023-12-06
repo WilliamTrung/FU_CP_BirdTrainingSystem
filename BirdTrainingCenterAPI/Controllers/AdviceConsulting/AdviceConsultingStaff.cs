d9c503b936272ac582f5a79d0abd29db3fa32fef
Bao <nguyenthothaibao@gmail.com>
2023-12-06 18:34:01 +0700

    Update API

--- BirdTrainingCenterAPI/Controllers/AdviceConsulting/AdviceConsultingStaff.cs
+++ BirdTrainingCenterAPI/Controllers/AdviceConsulting/AdviceConsultingStaff.cs
@@ -156,6 +156,8 @@ namespace BirdTrainingCenterAPI.Controllers.AdviceConsulting
             return Ok();
         }
 
+        [HttpGet]
+        [Route("preCalculateConsultantPrice")]
         public async Task<IActionResult> PreCalculateConsultantPrice(int ticketId, int distance)
         {
             var price = await _consultingService.Staff.PreCalculateConsultantPrice(ticketId, distance);
