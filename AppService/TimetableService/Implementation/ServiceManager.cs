dcf7a3c530dcc4f188ef45104dee915b8df2f546
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-12-19 22:07:55 +0700

    add check valid day to be logged

--- AppService/TimetableService/Implementation/ServiceManager.cs
+++ AppService/TimetableService/Implementation/ServiceManager.cs
@@ -20,11 +20,37 @@ namespace AppService.TimetableService.Implementation
         }
         public async Task LogAbsentInDay(AbsentInDayModel absentLog)
         {
+            //check time is 
+            if(!CheckTime(absentLog.SelectedDate))
+            {
+                throw new InvalidOperationException("Invalid logged time!");
+            }
             await _logAbsent.LogAbsentInDay(absentLog);
         }
         public async Task LogAbsentDateRange(AbsentDateRangeModel absentLog)
         {
+            if (!CheckTime(absentLog.From) || !CheckTime(absentLog.To))
+            {
+                throw new InvalidOperationException("Invalid logged time!");
+            }
             await _logAbsent.LogAbsentDateRange(absentLog);
+        }
+        private bool CheckTime(DateOnly checkDate)
+        {
+            // can only be logged freely within the whole month exclude the last 3 days of the month
+            //check within month
+            int currentMonth = DateTime.UtcNow.AddHours(7).Month;
+            if(currentMonth != checkDate.Month)
+            {
+                return false;
+            }
+            //check 3 last days in month
+            int daysInMonth = DateTime.DaysInMonth(checkDate.Year, checkDate.Month);
+            if(checkDate.Day > daysInMonth - 3)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
